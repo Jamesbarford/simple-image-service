8cb0d2701388c94c6eb9783e0ffaf4325af4c45a
Jamesbarford <Jamesbarfordevans@gmail.com>
2023-05-29 13:45:18 +0100

    remove logging module

--- image-service/src/logging/logger.module.ts
+++
@@ -1,8 +0,0 @@
-import {Module} from "@nestjs/common";
-import {ApplicationLogger} from "./logger";
-
-@Module({
-    providers: [ApplicationLogger],
-    exports: [ApplicationLogger],
-})
-export class LoggerModule {}
